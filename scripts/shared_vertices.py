59c7acf9b7c23b7fc9bee13ac9dd91b009c15285
Balázs Dukai <balazs.dukai@gmail.com>
2024-04-22 16:04:37 +0200

    Count shared vertices

---
+++ scripts/shared_vertices.py
@@ -0,0 +1,39 @@
+"""
+Script for counting the shared vertices.
+It writes a CSV file with the shared vertex count for each vertex, to the same
+directory as the input file.
+"""
+import json
+import sys
+from pathlib import Path
+import sys
+import csv
+
+if __name__ == '__main__':
+    inpath = Path(sys.argv[1]).resolve()
+
+    with inpath.open("r") as f:
+        data = json.load(f)
+
+    vertex_counts = {i: 0 for i in range(len(data['vertices']))}
+
+    for co_id, co in data["CityObjects"].items():
+        for geometry in co["geometry"]:
+            for surface in geometry["boundaries"]:
+                for ring in surface:
+                    for vertex in ring:
+                        vertex_counts[vertex] += 1
+
+    vertex_counts_sorted = dict(sorted(vertex_counts.items(), key=lambda item: item[1], reverse=True))
+
+    nr_shared = 0
+    with inpath.parent.joinpath(inpath.stem).with_suffix(".csv").open("w") as f:
+        csv_writer = csv.writer(f)
+        csv_writer.writerow(["Vertex", "Count"])
+        for vertex, count in vertex_counts_sorted.items():
+            if count > 1:
+                nr_shared += 1
+                csv_writer.writerow([vertex, count])
+
+    print(f"Nr. vertices: {len(vertex_counts)}")
+    print(f"Nr. shared vertices: {nr_shared}")
