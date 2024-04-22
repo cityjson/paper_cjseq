bb345ab16494be3c3bbd194591f48f9e6dabb8c1
Balázs Dukai <balazs.dukai@gmail.com>
2024-04-22 16:25:29 +0200

    Count shared vertices between cityobjects

--- scripts/shared_vertices.py
+++ scripts/shared_vertices.py
@@ -1,5 +1,5 @@
 """
-Script for counting the shared vertices.
+Script for counting how many CityObjects share a shared vertex.
 It writes a CSV file with the shared vertex count for each vertex, to the same
 directory as the input file.
 """
@@ -15,25 +15,33 @@ if __name__ == '__main__':
     with inpath.open("r") as f:
         data = json.load(f)
 
-    vertex_counts = {i: 0 for i in range(len(data['vertices']))}
+    vertex_counts = {i: set() for i in range(len(data['vertices']))}
 
     for co_id, co in data["CityObjects"].items():
         for geometry in co["geometry"]:
             for surface in geometry["boundaries"]:
                 for ring in surface:
                     for vertex in ring:
-                        vertex_counts[vertex] += 1
+                        vertex_counts[vertex].add(co_id)
 
-    vertex_counts_sorted = dict(sorted(vertex_counts.items(), key=lambda item: item[1], reverse=True))
+    vertex_counts_sorted = dict(sorted(vertex_counts.items(), key=lambda item: len(item[1]), reverse=True))
 
     nr_shared = 0
+    size_vertex = sys.getsizeof(data['vertices'][0])
+    total_size_vertices = size_vertex * len(data['vertices'])
+    total_size_vertices_unshared = 0
     with inpath.parent.joinpath(inpath.stem).with_suffix(".csv").open("w") as f:
         csv_writer = csv.writer(f)
         csv_writer.writerow(["Vertex", "Count"])
-        for vertex, count in vertex_counts_sorted.items():
-            if count > 1:
+        for vertex, ids in vertex_counts_sorted.items():
+            if len(ids) > 1:
                 nr_shared += 1
-                csv_writer.writerow([vertex, count])
+                total_size_vertices_unshared += size_vertex * len(ids)
+                csv_writer.writerow([vertex, len(ids)])
+            else:
+                total_size_vertices_unshared += size_vertex
 
     print(f"Nr. vertices: {len(vertex_counts)}")
     print(f"Nr. shared vertices: {nr_shared}")
+    print(f"Memory size vertices: {total_size_vertices} bytes")
+    print(f"Memory size vertices if none of them are shared: {total_size_vertices_unshared} bytes")
