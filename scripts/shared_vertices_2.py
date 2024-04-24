9918709e5234420de0f050a49b64683504381ba2
Hugo Ledoux <h.ledoux@tudelft.nl>
2024-04-24 15:24:43 +0200

    Fix script to calculate shared vertices

--- scripts/shared_vertices_2.py
+++ scripts/shared_vertices_2.py
@@ -4,26 +4,25 @@ import sys
 from pathlib import Path
 import sys
 import csv
+import glob
 
+fs = glob.glob('/Users/hugo/data/cityjson/CityJSONSeq-demo-files/*.json')
 
-inpath1 = Path("/Users/hugo/data/cityjson/3dcities/v2.0/VM05_2009.city.json").resolve()
-# inpath1 = Path("/Users/hugo/temp/r/Rotterdam_wo_tex.json").resolve()
-with inpath1.open("r") as f:
-    j = json.load(f)
-v_json = len(j["vertices"])
+for each in fs:
+    #-- json
+    with Path(each).resolve().open("r") as f:
+        j = json.load(f)
+    v_json = len(j["vertices"])
 
-inpath2 = Path("/Users/hugo/data/cityjson/3dcities/v2.0/VM05_2009.city.jsonl").resolve()
-# inpath2 = Path("/Users/hugo/temp/r/Rotterdam_wo_tex.jsonl").resolve()
-f = open(inpath2)
-v_jsonl = 0
-for (i, line) in enumerate(f.readlines()):
-    j2 = json.loads(line)
-    v_jsonl += len(j2["vertices"]) 
-            
+    #-- jsonl
+    eachl = each + "l"
+    f = open(eachl)
+    v_jsonl = 0
+    for (i, line) in enumerate(f.readlines()):
+        j2 = json.loads(line)
+        v_jsonl += len(j2["vertices"]) 
 
-# print(v_json)
-print(v_jsonl / v_json)
-print(v_json / v_jsonl)
+    print(each, v_jsonl / v_json)
 
 
 
